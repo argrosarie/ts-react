eb6e8ec4a07a47a82a8f31444b20c83724493c20
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-11 12:48:13 -0300

    Lazy loading connected

--- components/RandomCapy.tsx
+++ components/RandomCapy.tsx
@@ -1,8 +1,30 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 type Props = { image: string };
 
 export const RandomCapy = ({ image }: Props): JSX.Element => {
   const node = useRef<HTMLImageElement>(null);
-  return <img ref={node} width={320} height="auto" src={image} className="rounded" />;
+  const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+          setSrc(image)
+        }
+      })
+    })
+    if(node.current) {
+      observer.observe(node.current);
+    }
+   
+    return () => {
+      observer.disconnect()
+    }
+  },[image])
+  
+
+  
+
+  return <img ref={node} width={320} height="auto" src={src} className="rounded bg-gray-300" />;
 };
