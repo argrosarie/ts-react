38583292f4df7f5d3529f67d64cc747c7f4b9054
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-11 12:22:28 -0300

    useRef and null

--- components/RandomCapy.tsx
+++ components/RandomCapy.tsx
@@ -1,5 +1,8 @@
+import { useRef } from "react";
+
 type Props = { image: string };
 
 export const RandomCapy = ({ image }: Props): JSX.Element => {
-  return <img width={320} height="auto" src={image} className="rounded" />;
+  const node = useRef<HTMLImageElement>(null);
+  return <img ref={node} width={320} height="auto" src={image} className="rounded" />;
 };
