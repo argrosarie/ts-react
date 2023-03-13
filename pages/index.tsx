87da3a004941a465518244b85093f94c439f6a25
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-13 16:43:21 -0300

    Sticky bar with button

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,23 +44,26 @@ export default function Home() {
           experience without having to wait for all images to load at once.
         </p> */}
         {/* sticky top-0 z-20 */}
-       <div className="fixed top-0 left-0 right-0 flex justify-center bg-black">
+
+        <div className="fixed w-full flex justify-center flex-col items-center top-0 bg-[#F1BEDA]">
+          <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-wide  text-purple-700 sm:leading-10">
+            Capybara Generator
+          </h3>
           <button
-            className="p-3 mt-6 pointer text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900"
+            className="p-3 mt-6 pointer text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-4 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900"
             onClick={addNewCapy}
           >
             Add new Capy
           </button>
-        
-          </div>
-        <div className="flex items-center justify-center flex-wrap">
+        </div>
+        <div className="flex items-center justify-center flex-wrap mt-40">
           {images.map(({ id, url }) => (
             <div key={id} className="p-4">
               <LazyImage
                 width={320}
                 height="auto"
                 src={url}
-                className="rounded bg-gray-300"
+                className="rounded bg-gray-300 border border-[#F1BEDA]"
               />
             </div>
           ))}
